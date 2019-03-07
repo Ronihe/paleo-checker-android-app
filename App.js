2c4de6d81ee7bdcfe9af5b087108fa8d1fe93bee
Ronihe <herongrong2011@gmail.com>
2019-03-07 12:42:24 -0800

    first

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Take a food pic and check if your dish is paleo </Text>
+        {/* post your pic here */}
+        {/* ingredient and paleo fact table */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
