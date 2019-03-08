6b83700cae56de84b6d2f38a110af9db1a1f7e68
Ronihe <herongrong2011@gmail.com>
2019-03-07 16:19:19 -0800

    added base64

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+// import ImagePicker from 'react-native-image-picker';
 
-const options = {
-  title: 'Select Avatar',
-  customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
-  storageOptions: {
-    skipBackup: true,
-    path: 'images'
-  }
-};
+import { ImagePicker, Permissions } from 'expo';
+
+// const options = {
+//   title: 'Select Avatar',
+//   customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+//   storageOptions: {
+//     skipBackup: true,
+//     path: 'images'
+//   }
+// };
 
 // Launch Camera:
 
@@ -22,32 +24,50 @@ const options = {
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { avatarSource: undefined };
+    this.state = { image: undefined };
     // this.method = this.method.bind(this);
     // bind method here if necessary
   }
-  launchCamera = () => {
-    ImagePicker.launchCamera(options, response => {
-      // Same code as in above section!
 
-      console.log('Response = ', response);
+  _launchCamera = async () => {
+    //ask for permissons
+    const res = await Promise.all([
+      Permissions.askAsync(Permissions.CAMERA),
+      Permissions.askAsync(Permissions.CAMERA_ROLL)
+    ]);
 
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      } else {
-        const source = { uri: response.uri };
-
-        // You can also display the image using data:
-        // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-        this.setState({
-          avatarSource: source
-        });
-      }
+    // console.warn(ImagePicker);
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+      base64: true
     });
+    console.warn(result);
+    if (!result.cancelled) {
+      this.setState({ image: result.base64 });
+    }
+
+    // ImagePicker.launchCamera(options, response => {
+    //   // Same code as in above section!
+
+    //   console.log('Response = ', response);
+
+    //   if (response.didCancel) {
+    //     console.log('User cancelled image picker');
+    //   } else if (response.error) {
+    //     console.log('ImagePicker Error: ', response.error);
+    //   } else if (response.customButton) {
+    //     console.log('User tapped custom button: ', response.customButton);
+    //   } else {
+    //     const source = { uri: response.uri };
+
+    //     // You can also display the image using data:
+    //     // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+    //     this.setState({
+    //       avatarSource: source
+    //     });
+    //   }
+    // });
   };
 
   render() {
@@ -55,7 +75,11 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Take a food pic and check if your dish is paleo </Text>
         {/* post your pic here */}
-        <Image source={this.state.avatarSource} />
+        <Button onPress={this._launchCamera} title="picButton">
+          {' '}
+          Take a Pic{' '}
+        </Button>
+        {/* <Image source={require(`./${this.state.image}`)} /> */}
         {/* ingredient and paleo fact table */}
       </View>
     );
